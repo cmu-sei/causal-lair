26017b3c89cc87c230e285430e980b3b390e7a1b
David Shepard <daveman1010220@gmail.com>
2026-09-01 11:50:09 -0400

    chore(docs): Re-render the book

--- doc/air-book/book/searcher-09f2665d.js
+++ doc/air-book/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9f5a6a81.js',
+                path_to_root + 'searchindex-6dceaa6f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
